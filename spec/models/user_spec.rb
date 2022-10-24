d9659b9135293f189791a861dad048fd4b058247
Candace Eckels <candace.eckels@gmail.com>
2022-10-24 11:06:19 -0600

    Added bcrypt gem, validated user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :user_name }
+    it { should validate_uniqueness_of :user_name }
     it { should validate_presence_of :email }
-    # it { should validate_presence_of :password_digest }
+    it { should validate_presence_of :password_digest }
     it { should validate_uniqueness_of :email }
+    it { should have_secure_password }
+
   end
 
   describe 'relationships' do
